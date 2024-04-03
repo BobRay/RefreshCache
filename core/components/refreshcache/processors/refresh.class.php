719457b834ca40e787afa36d83ed2fb63981624d
bobray <bobray@softville.com>
2024-04-02 22:31:31 -0500

    Imported from BG; use our own getResource()

--- core/components/refreshcache/processors/refresh.class.php
+++ core/components/refreshcache/processors/refresh.class.php
@@ -7,7 +7,7 @@ if (!defined('MODX_CORE_PATH')) {
     /* For dev env. */
     @include_once dirname(__FILE__, 7) . '/config.core.php';
 
-    if (! defined('MODX_CORE_PATH')) {
+    if (!defined('MODX_CORE_PATH')) {
         @include dirname(__FILE__, 4) . '/config.core.php';
     }
 }
@@ -59,8 +59,7 @@ class refreshcacheRefreshProcessor extends tempRCprocessor {
         /* Set context to prevent caching in 'mgr' dir */
         $this->modx->context->set('key', $context);
 
-        $doc = $this->modx->getObject($this->prefix . 'modResource', $id);
-
+        $doc = $this->getResource('id', $id);
         $delay = $this->modx->getOption('refreshcache_request_delay',
             null, 0, true);
         usleep((int)$delay * 1000);
@@ -70,8 +69,6 @@ class refreshcacheRefreshProcessor extends tempRCprocessor {
         $this->modx->resource = $doc;
         $displayErrors = ini_set('display_errors', 0);
         $errorLevel = error_reporting(0);
-
-        $this->refreshResource_generateResource($doc);
 
         /* Restore error settings and context */
         if ($displayErrors) {
@@ -83,28 +80,131 @@ class refreshcacheRefreshProcessor extends tempRCprocessor {
         return (json_encode((array('success' => true))));
     }
 
-    public function refreshResource_generateResource(&$doc, $options = array()) {
-        /* Options for generateResource call */
-        $options = array(
-            /*xPDO::OPT_CACHE_HANDLER => $this->modx->getOption
-           (xPDO::OPT_CACHE_DB_HANDLER, null, $this->modx->getOption(xPDO::OPT_CACHE_HANDLER, null, 'xPDO\\Cache\\xPDOFileCache')),*/
+    /**
+     * Modified version of this method in the modRequest class.
+     * Gets a requested resource and all required data.
+     *
+     * @param string $method The method, 'id', or 'alias', by which to perform
+     * the resource lookup.
+     * @param string|integer $identifier The identifier with which to search.
+     * @param array $options An array of options for the resource fetching
+     * @return modResource The requested modResource instance or request
+     * is forwarded to the error page, or unauthorized page.
+     */
 
+    public function getResource($method, $identifier, array $options = array()) {
+        /* First two args should always be 'id', and Resource ID (int) */
+        /** @var modContentType $contentType */
+        $resource = null;
+        if ($method == 'alias') {
+            die('method should always be "id"');
+        } else {
+            $resourceId = (int)$identifier;
+        }
 
-            xPDO::OPT_CACHE_KEY => $this->modx->getOption
-            ('cache_resource_key', null, 'resource'),
+        if (!is_numeric($resourceId)) {
+            $this->modx->sendErrorPage();
+        }
+        $isForward = false;
+        $fromCache = false;
 
-            xPDO::OPT_CACHE_HANDLER => $this->modx->getOption('cache_handler', null,
-                $this->modx->getOption(xPDO::OPT_CACHE_HANDLER)),
+        $cacheKey = $this->modx->context->get('key') . "/resources/{$resourceId}";
+        $cachedResource = $this->modx->cacheManager->get($cacheKey, array(
+            xPDO::OPT_CACHE_KEY => $this->modx->getOption('cache_resource_key', null, 'resource'),
+            xPDO::OPT_CACHE_HANDLER => $this->modx->getOption('cache_resource_handler', null, $this->modx->getOption(xPDO::OPT_CACHE_HANDLER)),
+            xPDO::OPT_CACHE_FORMAT => (integer)$this->modx->getOption('cache_resource_format', null, $this->modx->getOption(xPDO::OPT_CACHE_FORMAT, null, xPDOCacheManager::CACHE_PHP)),
+        ));
+        if (is_array($cachedResource) && array_key_exists('resource', $cachedResource) && is_array($cachedResource['resource'])) {
+            /** @var modResource $resource */
+            $resource = $this->modx->newObject($cachedResource['resourceClass']);
+            if ($resource) {
+                $resource->fromArray($cachedResource['resource'], '', true, true, true);
+                $resource->_content = $cachedResource['resource']['_content'];
+                $resource->_isForward = $isForward;
+                if (isset($cachedResource['contentType'])) {
+                    $contentType = $this->modx->newObject($this->prefix . 'modContentType');
+                    $contentType->fromArray($cachedResource['contentType'], '', true, true, true);
+                    $resource->addOne($contentType, 'ContentType');
+                }
+                if (isset($cachedResource['resourceGroups'])) {
+                    $rGroups = array();
+                    foreach ($cachedResource['resourceGroups'] as $rGroupKey => $rGroup) {
+                        $rGroups[$rGroupKey] = $this->modx->newObject('modResourceGroupResource', $rGroup);
+                    }
+                    $resource->addMany($rGroups);
+                }
+                if (isset($cachedResource['policyCache'])) {
+                    $resource->setPolicies(array($this->modx->context->get('key') => $cachedResource['policyCache']));
+                }
+                if (isset($cachedResource['elementCache'])) {
+                    $this->modx->elementCache = $cachedResource['elementCache'];
+                }
+                if (isset($cachedResource['sourceCache'])) {
+                    $this->modx->sourceCache = $cachedResource['sourceCache'];
+                }
+                if ($resource->get('_jscripts')) {
+                    $this->modx->jscripts = $this->modx->jscripts + $resource->get('_jscripts');
+                }
+                if ($resource->get('_sjscripts')) {
+                    $this->modx->sjscripts = $this->modx->sjscripts + $resource->get('_sjscripts');
+                }
+                if ($resource->get('_loadedjscripts')) {
+                    $this->modx->loadedjscripts = array_merge($this->modx->loadedjscripts, $resource->get('_loadedjscripts'));
+                }
+                $isForward = $resource->_isForward;
+                // $resource->setProcessed(true);
+                $fromCache = true;
+            }
+        }
+        if (!$fromCache || !is_object($resource)) {
+            $criteria = $this->modx->newQuery('modResource');
+            $criteria->select(array($this->modx->escape('modResource') . '.*'));
+            $criteria->where(array('id' => $resourceId));
 
-            xPDO::OPT_CACHE_FORMAT => (integer)$this->modx->getOption('cache_format', null,
-                $this->modx->getOption(xPDO::OPT_CACHE_FORMAT, null, xPDOCacheManager::CACHE_PHP)),
+            if ($resource = $this->modx->getObject($this->prefix . 'modResource', $criteria)) {
+                if ($resource) {
+                    if ($resource->get('context_key') !== $this->modx->context->get('key')) {
+                        if (!$isForward || ($isForward && !$this->modx->getOption('allow_forward_across_contexts', $options, false))) {
+                            if (!$this->modx->getCount('modContextResource', array($this->modx->context->get('key'), $resourceId))) {
+                                return null;
+                            }
+                        }
+                    }
+                    $resource->_isForward = $isForward;
 
-            xPDO::OPT_CACHE_PREFIX => '',
-        );
-
+                    if ($tvs = $resource->getMany('TemplateVars', 'all')) {
+                        /** @var modTemplateVar $tv */
+                        foreach ($tvs as $tv) {
+                            $resource->set($tv->get('name'), array(
+                                $tv->get('name'),
+                                $tv->getValue($resource->get('id')),
+                                $tv->get('display'),
+                                $tv->get('display_params'),
+                                $tv->get('type'),
+                            ));
+                        }
+                    }
+                    $this->modx->resourceGenerated = true;
+                }
+            }
+        } elseif ($fromCache && false) { // should be unnecessary
+            if (($resource->get('published') || ($this->modx->getSessionState() === modX::SESSION_STATE_INITIALIZED && $this->modx->hasPermission('view_unpublished')))) {
+                if ($resource->get('context_key') !== $this->modx->context->get('key')) {
+                    if (!$isForward || ($isForward && !$this->modx->getOption('allow_forward_across_contexts', $options, false))) {
+                        if (!$this->modx->getCount('modContextResource', array($this->modx->context->get('key'), $resourceId))) {
+                            return null;
+                        }
+                    }
+                }
+            } else {
+                return null;
+            }
+        }
+        $resource->setProcessed(true);
         $cm = $this->modx->getCacheManager();
-       $x =  @$doc->process();
-       $y =  @$cm->generateResource($doc, $options);
+        @$cm->generateResource($resource);
+
+        return $resource;
     }
 }
 
